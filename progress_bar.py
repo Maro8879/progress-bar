acc33910a6137b767710f42e8c053817d6f28ca7
Maro8879 <97912212+Maro8879@users.noreply.github.com>
2022-02-06 16:50:31 +0100

    Create progress_bar.py

---
+++ progress_bar.py
@@ -0,0 +1,18 @@
+from os import system
+from os import name
+from math import floor
+
+
+def progress_bar(data, process=None, cls=True, color='92'):
+    system('color')
+    elem_counter = 0
+    print(f'\033[1;{color}m{0:.2f}% | [{"█" * floor(0) + "░" * int(100 - floor(0))}]\033[0;0m')
+    for elem in data:
+        # process
+        process(elem) if process is not None else None
+        elem_counter += 1
+        percent = elem_counter/len(data) * 100
+        # loading bar
+        system('cls' if name == 'nt' else 'clear') if cls is True else ''
+        print(f'\033[1;{color}m{percent:.2f}% | [{"█" * floor(percent) + "░" * int(100 - floor(percent))}]\033[0;0m')
+
